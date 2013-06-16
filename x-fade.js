6e9bc584c190f5532ae734266086f3b491668924
MattSmiglarski <matt.smiglarski@gmail.com>
2013-06-16 17:45:08 +0000

    Hotlink sample images; remove stuff from demo; fix flash of unstyled content.

--- x-fade.js
+++ x-fade.js
@@ -1,26 +1,24 @@
-function setClass(el, classAttribute) { el.className = classAttribute;}
+function slideshow(elArray, p) {
 
-function slideshow1(elArray, p) {
-
+    p = p || {};
     var elWrapper;
-    var i=0, j, currentWidth =0, currentHeight = 0;
+    var i=0, j, currentWidth = 0, currentHeight = 0;
     var tempImage;
 
     var loop = function() {
 	tempImage = elWrapper.childNodes[i];
-	setClass(elWrapper.childNodes[i], "");
+	elWrapper.childNodes[i].className = "";
 	i += 1;
 	if (i === elWrapper.childNodes.length) i = 0;
-	setClass(elWrapper.childNodes[i], "top");
-	setTimeout(loop, p.delay);
+	elWrapper.childNodes[i].className ="top";
+	setTimeout(loop, p.delay || 4000);
     }
 
     function init() {
 	elWrapper = document.createElement("div");
 	elWrapper.width = 0;
 	elWrapper.height = 0;
-	setClass(elWrapper, "cf");
-	console.log(p.target);
+	elWrapper.className = "cf";
 	(p.target || document.body).appendChild(elWrapper);
 
 	function elementReady(el) {
@@ -34,17 +32,19 @@ function slideshow1(elArray, p) {
 		img.style.left = (currentWidth - img.width)/2  + 'px';
 		img.style.top = (currentHeight - img.height)/2 + 'px';
 	    }
+            el.style.display = 'block';
 	}
 
 	for (j=0; j<elArray.length; j++) {
 	    tempImage = document.createElement("img");
+            tempImage.style.display = 'none';
 	    tempImage.onload = (function(el) {
 		return function() { elementReady(el); }
 	    })(tempImage);
-	    elWrapper.appendChild(tempImage);
+            elWrapper.appendChild(tempImage);
 	    tempImage.src = elArray[j];
 	}
-     }
+    }
 
     init();
     loop();
